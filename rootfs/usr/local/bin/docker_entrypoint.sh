18f60c8f800adf5b70bad7810a028a9173f55254
stlouisn <stlouisn@hotmail.com>
2018-03-11 14:51:25 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
-# Make sure volumes are mounted correctly
-if [[ ! -d /var/lib/airsonic ]]; then
-    echo -e "\nError: volume '/var/lib/airsonic/' not mounted.\n" >&2
+# Fix user and group ownerships for '/config'
+if [[ ! -d /config ]]; then
+    echo -e "\nError: volume '/config' not mounted.\n" >&2
     exit 1
-elif [[ `mount | grep '/var/lib/airsonic' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nError: volume '/var/lib/airsonic/' is readonly.\n" >&2
+elif [[ `mount | grep '/config' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
+    echo -e "\nError: volume '/config' is readonly.\n" >&2
     exit 1
+else
+    chown -R airsonic:airsonic /config
 fi
 
-# Fix user and group ownerships
-chown -R airsonic:airsonic /var/lib/airsonic
-
-# Fix user and group ownerships
+# Fix user and group ownerships for '/music'
 if [[ ! -d /music ]]; then
-    echo -e "\nWarning: volume '/music/' not mounted.\n" >&2
+    echo -e "\nWarning: volume '/music' not mounted.\n" >&2
 elif [[ `mount | grep '/music' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nWarning: volume '/music/' is readonly.\n" >&2
+    echo -e "\nWarning: volume '/music' is readonly.\n" >&2
 else
     chown -R airsonic:airsonic /music
 fi
 
-# Fix user and group ownerships
+# Fix user and group ownerships for '/playlists'
 if [[ ! -d /playlists ]]; then
-    echo -e "\nWarning: volume '/playlists/' not mounted.\n" >&2
+    echo -e "\nWarning: volume '/playlists' not mounted.\n" >&2
 elif [[ `mount | grep '/playlists' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nWarning: volume '/playlists/' is readonly.\n" >&2
+    echo -e "\nWarning: volume '/playlists' is readonly.\n" >&2
 else
     chown -R airsonic:airsonic /playlists
 fi
 
-# Fix user and group ownerships
+# Fix user and group ownerships for '/podcasts'
 if [[ ! -d /podcasts ]]; then
-    echo -e "\nWarning: volume '/podcasts/' not mounted.\n" >&2
+    echo -e "\nWarning: volume '/podcasts' not mounted.\n" >&2
 elif [[ `mount | grep '/podcasts' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nWarning: volume '/podcasts/' is readonly.\n" >&2
+    echo -e "\nWarning: volume '/podcasts' is readonly.\n" >&2
 else
     chown -R airsonic:airsonic /podcasts
 fi
@@ -47,7 +46,7 @@ exec gosu airsonic /usr/bin/java \
     -Dairsonic.defaultMusicFolder=/music \
     -Dairsonic.defaultPlaylistFolder=/playlists \
     -Dairsonic.defaultPodcastFolder=/podcasts \
-    -Dairsonic.home=/var/lib/airsonic \
+    -Dairsonic.home=/config \
     -Djava.awt.headless=true \
     -Dserver.host=0.0.0.0 \
     -Dserver.port=4040 \
