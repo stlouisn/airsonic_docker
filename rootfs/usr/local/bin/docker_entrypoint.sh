99fadbab70e0ce01dd0493240215ee58c204ebfa
stlouisn <stlouisn@hotmail.com>
2018-03-11 18:17:24 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
 
-# Fix user and group ownerships for '/config'
-if [[ ! -d /config ]]; then
-    echo -e "\nError: volume '/config' not mounted.\n" >&2
+# Fix user and group ownerships for '/var/lib/airsonic'
+if [[ ! -d /var/lib/airsonic ]]; then
+    echo -e "\nError: volume '/var/lib/airsonic' is not mounted.\n" >&2
     exit 1
-elif [[ `mount | grep '/config' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nError: volume '/config' is readonly.\n" >&2
+elif [[ `mount | grep '/var/lib/airsonic' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
+    echo -e "\nError: volume '/var/lib/airsonic' is readonly.\n" >&2
     exit 1
 else
-    chown -R airsonic:airsonic /config
+    chown -R airsonic:airsonic /var/lib/airsonic
 fi
 
 # Fix user and group ownerships for '/music'
-if [[ ! -d /music ]]; then
-    echo -e "\nWarning: volume '/music' not mounted.\n" >&2
-elif [[ `mount | grep '/music' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nWarning: volume '/music' is readonly.\n" >&2
-else
+if [[ `mount | grep '/music' | awk -F '(' {'print $2'} | cut -c -2` == "rw" ]]; then
     chown -R airsonic:airsonic /music
 fi
 
 # Fix user and group ownerships for '/playlists'
-if [[ ! -d /playlists ]]; then
-    echo -e "\nWarning: volume '/playlists' not mounted.\n" >&2
-elif [[ `mount | grep '/playlists' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nWarning: volume '/playlists' is readonly.\n" >&2
-else
+if [[ `mount | grep '/playlists' | awk -F '(' {'print $2'} | cut -c -2` == "rw" ]]; then
     chown -R airsonic:airsonic /playlists
 fi
 
 # Fix user and group ownerships for '/podcasts'
-if [[ ! -d /podcasts ]]; then
-    echo -e "\nWarning: volume '/podcasts' not mounted.\n" >&2
-elif [[ `mount | grep '/podcasts' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nWarning: volume '/podcasts' is readonly.\n" >&2
-else
+if [[ `mount | grep '/podcasts' | awk -F '(' {'print $2'} | cut -c -2` == "rw" ]]; then
     chown -R airsonic:airsonic /podcasts
 fi
 
@@ -46,7 +34,7 @@ exec gosu airsonic /usr/bin/java \
     -Dairsonic.defaultMusicFolder=/music \
     -Dairsonic.defaultPlaylistFolder=/playlists \
     -Dairsonic.defaultPodcastFolder=/podcasts \
-    -Dairsonic.home=/config \
+    -Dairsonic.home=/var/lib/airsonic \
     -Djava.awt.headless=true \
     -Dserver.host=0.0.0.0 \
     -Dserver.port=4040 \
