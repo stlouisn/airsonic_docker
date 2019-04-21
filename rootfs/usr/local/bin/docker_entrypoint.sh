9860fae316242410336340953966b39212772b74
stlouisn <stlouisn@hotmail.com>
2019-04-21 11:49:08 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -8,28 +8,22 @@ chown -R airsonic:airsonic /config
 # Fix user and group ownerships for '/music'
 if [[ `mount | grep '/music' | awk -F '(' {'print $2'} | cut -c -2` == "rw" ]]; then
     chown -R airsonic:airsonic /music
-else
-    echo -e "\nWarning: volume '/music' is readonly.\n" >&2
 fi
 
 # Fix user and group ownerships for '/playlists'
 if [[ `mount | grep '/playlists' | awk -F '(' {'print $2'} | cut -c -2` == "rw" ]]; then
     chown -R airsonic:airsonic /playlists
-else
-    echo -e "\nWarning: volume '/playlists' is readonly.\n" >&2
 fi
 
 # Fix user and group ownerships for '/podcasts'
 if [[ `mount | grep '/podcasts' | awk -F '(' {'print $2'} | cut -c -2` == "rw" ]]; then
     chown -R airsonic:airsonic /podcasts
-else
-    echo -e "\nWarning: volume '/podcasts' is readonly.\n" >&2
 fi
 
 #=========================================================================================
 
 # Change workdir
-cd /usr/lib/airsonic
+cd /airsonic/
 
 # Start airsonic in console mode
 exec gosu airsonic \
@@ -42,4 +36,4 @@ exec gosu airsonic \
     -Dserver.host=0.0.0.0 \
     -Dserver.port=4040 \
     -Xmx512m \
-    -jar /usr/lib/airsonic/airsonic.war
+    -jar /airsonic/airsonic.war
